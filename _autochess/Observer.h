97959d6e69566bd514bb8a74437d70c30581015d
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2019-01-05 00:56:42 -0800

    added subject class

--- _autochess/Observer.h
+++ _autochess/Observer.h
@@ -1,6 +1,11 @@
 #ifndef _observer
 #define _observer
 
+#include <vector>
+#include <iostream>
+#include <string>
+using namespace std;
+
 class ChessCommunicator;
 
 class Observer {
@@ -8,5 +13,16 @@ public:
 	virtual void update() = 0;
 protected:
 	ChessCommunicator* communicator;
+};
+
+class Subject {
+public:
+	string getFen();
+	void setFen(string);
+	void attach(Observer*);
+	void notifyAllObservers();
+private:
+	vector<Observer> observers;
+	string fen;
 };
 #endif
