b52452f100f55884d65ac893322f196d299351e0
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2019-01-05 00:06:33 -0800

    added chesscommunicator header

--- _autochess/chesscomm.h
+++ _autochess/chesscomm.h
@@ -1,7 +1,28 @@
 #ifndef _chesscomm
 #define _chesscom
 
+#include <Windows.h>
+
+class MoveStrategy;
+
 class ChessCommunicator {
-	
+public:
+	virtual void MovePiece() = 0;
+};
+
+class EngineCommunicator: public ChessCommunicator {
+public:
+	EngineCommunicator(MoveStrategy*, TCHAR*, TCHAR*);
+	void MovePiece();
+private:
+	MoveStrategy* strategy;
+};
+
+class GraphicalCommunicator : public ChessCommunicator {
+public:
+	GraphicalCommunicator(MoveStrategy*, TCHAR*, TCHAR*);
+	void MovePiece();
+private:
+	MoveStrategy* strategy;
 };
 #endif
